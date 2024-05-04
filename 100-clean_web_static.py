e2164311cedae07f6615f282513595e8dad27dee
ultimatekaycee <ultimatekaycee@outlook.com>
2024-05-04 14:22:03 -0700

    rechecked two

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,16 +7,16 @@ env.hosts = ['34.201.165.113', '54.90.52.235']
 env.user = "ubuntu"
 
 
-def do_clean(numb=0):
+def do_clean(number=0):
     """ A Function that will clean """
 
-    numb = int(numb)
+    number = int(number)
 
-    if numb == 0:
-        numb = 2
+    if number == 0:
+        number = 2
     else:
-        numb += 1
+        number += 1
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(numb))
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, numb))
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
